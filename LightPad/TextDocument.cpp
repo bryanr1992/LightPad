bb216d91844b3c044f0ede6c444308e7a9022844
Bryan Rivas <bryan.rivas05@gmail.com>
2024-12-17 19:27:07 -0500

    Fixed some compile time errors TODO: Class Constructor/Destructor

--- LightPad/TextDocument.cpp
+++ LightPad/TextDocument.cpp
@@ -2,7 +2,7 @@
 #include "TextDocumentInternal.h"
 
 
-bool TextDocument::init(char* filename)
+bool TextDocument::init(TCHAR* filename)
 {
 	HANDLE hFile;
 	hFile = CreateFile(filename, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
@@ -12,14 +12,14 @@ bool TextDocument::init(char* filename)
 	return init(hFile);
 }
 
-bool TextDocument::init(HANDLE hfile)
+bool TextDocument::init(HANDLE hFile)
 {
 	ULONG numread;
 
-	if ((length = GetFileSize(hfile, 0)) == 0) { return false; }
+	if ((TextDocument::length = GetFileSize(hFile, 0)) == 0) { return false; }
 
 	// allocate a new buffer for the file
-	if ((buffer = new char[length]) == 0) { return false; }
+	if ((TextDocument::buffer = new char[TextDocument::length]) == 0) { return false; }
 
 	// read file into memory
 	ReadFile(hFile, buffer, length, &numread, 0);
--- LightPad/TextDocumentInternal.h
+++ LightPad/TextDocumentInternal.h
@@ -3,7 +3,8 @@
 class TextDocument
 {
 public:
-	bool init(char* filename);
+	bool init(TCHAR* filename);
+	bool init(HANDLE hFile);
 
 	ULONG getline(ULONG lineno, char* buf, size_t len);
 	ULONG linecount();
